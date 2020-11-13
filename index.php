e51ed5c89db2ce49f99ea919f3c1dd52d440325d
63070093 <73688511+63070093@users.noreply.github.com>
2020-11-13 15:05:11 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>ITF Lab</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
@@ -10,9 +10,6 @@
 </head>
 
 <html>
-<head>
-<title>ITF Lab</title>
-</head>
 <body>
 <?php
 $conn = mysqli_init();
